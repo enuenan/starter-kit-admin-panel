f956f635761d0844cd120639d3c423d7ee9d8fc3
Moammer Farshid Enan <enan@spaceship.com.sg>
2025-07-29 23:31:41 +0600

    Update input field component to show password if the field type is password

--- resources/views/components/forms/input.blade.php
+++ resources/views/components/forms/input.blade.php
@@ -32,14 +32,19 @@
 
     </label>
 @endif
-<div>
-    {{-- <input type="{{ $type }}" id="{{ $name }}" placeholder="{{ $finalPlaceholder }}" name="{{ $name }}"
-    {{ $attributes->merge(['class' => 'w-full px-4 py-2 rounded-lg text-gray-700 dark:text-gray-300 bg-gray-50 dark:bg-gray-700 border border-gray-300 dark:border-gray-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent']) }}> --}}
 
+<div class="relative">
     <input type="{{ $type }}" id="{{ $name }}" placeholder="{{ $finalPlaceholder }}"
         name="{{ $name }}" {{ $required ? 'required' : '' }}
-        {{ $attributes->merge(['class' => $defaultClass . ' w-full']) }}
+        {{ $attributes->merge(['class' => $defaultClass . ' w-full pr-10']) }}
         @if ($type == 'file') onchange="previewFile(this);" @endif />
+
+    @if ($type === 'password')
+        <button type="button" onclick="togglePassword('{{ $name }}')"
+            class="absolute right-3 top-2.5 text-gray-500 dark:text-gray-300 focus:outline-none">
+            <i class="fa-solid fa-eye" id="eye-icon-{{ $name }}"></i>
+        </button>
+    @endif
 
     @error($name)
         <span class="text-red-500 text-md">{{ $message }}</span>
@@ -47,4 +52,23 @@
 </div>
 @if ($file || $type == 'file')
     <img src="{{ $file ?: 'https://placehold.co/200x200' }}" class="w-50 h-50 mt-2" id="previewImg" alt="">
+@endif
+
+@if ($type === 'password')
+    <script>
+        function togglePassword(fieldId) {
+            const input = document.getElementById(fieldId);
+            const icon = document.getElementById('eye-icon-' + fieldId);
+
+            if (input.type === "password") {
+                input.type = "text";
+                icon.classList.remove('fa-eye');
+                icon.classList.add('fa-eye-slash');
+            } else {
+                input.type = "password";
+                icon.classList.remove('fa-eye-slash');
+                icon.classList.add('fa-eye');
+            }
+        }
+    </script>
 @endif
